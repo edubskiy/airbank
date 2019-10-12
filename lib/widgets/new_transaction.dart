486e1c198df6bce810c7fab08c791343f623da05
Evgeniy <mr.engineer.x@gmail.com>
2019-10-12 17:10:07 +0300

    Code refactor and some basic input validation

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
   final Function addTransaction;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   NewTransaction(this.addTransaction);
 
+  void submitData() {
+    var title = titleController.text;
+    var amount = double.parse(amountController.text);
+
+    if (title.isEmpty || amount < 0) {
+      return;
+    }
+
+    addTransaction(title, amount);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final titleController = TextEditingController();
-    final amountController = TextEditingController();
-
+    
     return Card(
       elevation: 5,
       child: Container(
@@ -22,13 +33,15 @@ class NewTransaction extends StatelessWidget {
               controller: titleController,
             ),
             TextField(
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text('Add transaction'),
               textColor: Colors.purple,
-              onPressed: () => addTransaction(titleController.text, double.parse(amountController.text)),
+              onPressed: submitData,
             )
           ],
         ),
