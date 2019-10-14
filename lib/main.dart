176248e20e1654d138c9f12df2e8201987931dc2
Evgeniy <edubskiy@openwaygroup.com>
2019-10-14 19:44:19 +0300

    Added no transaction image

--- lib/main.dart
+++ lib/main.dart
@@ -59,18 +59,18 @@ class _HomePageState extends State<HomePage> {
   }
 
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New shoes',
-      amount: 69.99,
-      date: DateTime.now()
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly casual groceries',
-      amount: 14.66,
-      date: DateTime.now()
-    )
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now()
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Weekly casual groceries',
+    //   amount: 14.66,
+    //   date: DateTime.now()
+    // )
   ];
 
   void addNewTransactionPanel(BuildContext ctx) {
