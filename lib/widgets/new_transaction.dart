f3289084ebbadcdca1d17dabccdc111770957832
Evgeniy <edubskiy@openwaygroup.com>
2019-10-21 17:21:42 +0300

    Added AdaptiveButton widget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:airbank/widgets/adaptive_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -85,18 +86,7 @@ class _NewTransactionState extends State<NewTransaction> {
                           ? 'No date chosen'
                           : "Chosen date:${DateFormat.yMMMd().format(_selectedDate)}"),
                     ),
-                    Platform.isIOS 
-                    ? CupertinoButton(
-                        child: Text('Choose date',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                        onPressed: _showDatePicker,
-                      ) 
-                    : FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      child: Text('Choose date',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      onPressed: _showDatePicker,
-                    )
+                    AdaptiveButton('Choose date', _showDatePicker)
                   ],
                 ),
               ),
